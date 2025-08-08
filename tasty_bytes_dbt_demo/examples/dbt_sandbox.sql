6b54d76b5aa2915bda4edc92f5c9ee5927ef6e37
ctc-naoya-takayama <naoya.takayama@ctc-g.co.jp>
2025-08-08 05:09:54 +0000

    fix

--- tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
+++ tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
@@ -1,2 +1,3 @@
 SHOW TABLES IN DATABASE tasty_bytes_dbt_db;
 SHOW VIEWS IN DATABASE tasty_bytes_dbt_db;
+EXECUTE DBT PROJECT TASTY_BYTES_DBT_DB.DEV.tasty_bytes_dbt_project ARGS='run --select customer_loyalty_metrics --target dev';
